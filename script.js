5819cbd9bf979ef628a46692cb4c97112be740f8
adilmarghadi1 <adilmarghadi2020@gmail.com>
2021-12-28 13:44:01 +0000

    add more tutorial

--- script.js
+++ script.js
@@ -244,3 +244,406 @@ while (number>50)
 
 alert("Good Bro")
 
+
+// Problem statement#
+// Write a program that asks the user for a number, then show the multiplication table for this number.
+
+
+let user = Number(prompt("Enter a number "))
+
+for (let i=1; i<=10; i++)
+{
+    console.log(`${user} * ${i} = ${user * i}`)
+}
+
+
+
+// ---------------------- //
+
+// Coding Challenge: Neither Yes nor No
+// Learn how to write a code that doesn't stop prompting for user input unless the user enters 'y' or 'n'. Good luck!
+
+
+let user = prompt("")
+while ((user != 'n') && (user != 'y'))
+{
+    user = prompt("")
+}
+
+console.log("You just lost!")
+
+
+
+
+
+// -------------------------------------------//
+
+
+// Problem statement#
+// Write a program that shows all numbers between 11 and 100100 with the following exceptions:
+
+// If the number is divisible by 3, it should display Fizz
+// If the number is divisible by 5, it should display Buzz
+// If the number is divisible both by 5 and by 3, it should display FizzBuzz
+
+
+for (let i =1; i<=100; i++)
+{
+    if (i % 3 === 0)
+        console.log("Fizz")
+    else if (i % 5 === 0)
+        console.log("Buzz")
+
+    else if (i % 3 === 0 && i % 5 === 0)
+        console.log("FizzBuzz")
+    
+    else
+        console.log(i)
+}
+
+
+
+// --------------------------------------------//
+
+// Funtions
+
+function presentation(name, age)
+{
+    console.log(`My name is ${name} and my age is ${age} `)
+}
+
+presentation("adil", 21)
+
+
+// ANother way to declare a function
+
+const hello = function(name) {
+    const message = `Hello ${name}!`
+    return message;
+}
+console.log(hello("adil"))
+
+
+const hello = (name) => {
+    const message = `Hello ${name}`
+    return message;
+}
+
+console.log(hello("adil"))
+
+// ---------//
+
+function hello(firstName, lastName) {
+    firstName = prompt("Enter first name ")
+    lastName = prompt("Enter second name ")
+
+}
+hello()
+
+
+// ----------------------------------- //
+
+// Problem statement#
+// Complete the below program so that the min() function returns the minimum of its two received numbers.
+
+function min(a,b)
+{
+    if (a<b)
+    {
+        console.log(a)
+    }
+
+    else {
+        console.log(b)
+    }
+}
+
+min(2,3)
+
+
+// ----------------------------- //
+
+
+// Problem statement#
+// Complete the below program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
+
+
+function calculate(a, operation, b)
+{
+    if (operation === '+')
+    {
+        let sum = a + b;
+        console.log(sum)
+    }
+    else if (operation === '-')
+    {
+        let diff = a - b;
+        console.log(diff)
+    }
+
+    else if (operation === '*')
+    {
+        let mul = a * b;
+        console.log(mul)
+    }
+
+    else if (operation === '/')
+    {
+        let divi = a / b;
+        console.log(divi)
+    }
+
+    else {
+        console.log("I don't Know")
+    }
+
+    
+}
+
+calculate(3, '+', 4)
+
+// swicth
+function calculate(num1,operator,num2){
+		switch(operator){
+      case "+":
+        	console.log(num1 + num2);
+        break;
+        
+      case "-":
+        	console.log(num1 - num2);
+        break;
+        
+      case "*":
+        	console.log(num1 * num2);
+        break; 
+        
+      case "/":
+        	console.log(num1 / num2);
+        break;        
+      default:
+       console.log(undefined);
+    	}
+}
+
+calculate(12,'+',2);
+calculate(12,'-',2);
+calculate(12,'*',2);
+calculate(12,'/',2);
+
+
+
+
+// ---------------------------//
+
+// Objects
+
+const pen = {
+    color:"blue",
+    brand : "bic"
+}
+
+pen.price = "2.2"
+console.log(`My pen is ${pen.brand} and it cost ${pen.price}`)
+
+
+
+// Problem statement#
+// Create a Dog object with the following properties:
+
+// name
+// species
+// size
+// describe() - display all attributes of an object
+// bark() - returns “Woof!”
+
+
+var dog = {
+	name: 'Tim',
+  species: 'German Shepherd',
+  size: '3 foot',
+  bark(){
+		console.log("Woof!");
+	},
+  describe(){
+      console.log(`${this.name} belongs to ${this.species} species and its size is ${this
+      .size}`);
+  }
+};
+dog.bark();
+dog.describe();
+
+ // ---------------------------- //
+
+//  Arrays
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+console.log(movies.length); // 3
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+console.log(movies[0]); // "The Wolf of Wall Street"
+console.log(movies[1]); // "Zootopia"
+console.log(movies[2]); // "Babysitting"
+
+for (let i=0; i<movies.length - 1; i++)
+{
+    console.log(movies[i])
+}
+
+movies.forEach(movie => {
+    console.log(movie)
+})
+
+
+for (let movie of movies)
+{
+    console.log(movie)
+}
+
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+movies.unshift("Pacific Rim");
+console.log(movies[0]); // "Pacific Rim"
+
+
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+movies.pop(); // Remove the last array element
+console.log(movies.length); // 2
+console.log(movies[2]); // undefined
+
+
+
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+movies.splice(0, 1); // Remove 1 element starting at index 0
+console.log(movies.length); // 2
+console.log(movies[0]); // "Zootopia"
+console.log(movies[1]); // "Babysitting"
+
+
+
+
+// ------------------------------------------------//
+
+// Problem statement#
+
+// Create an array named musketeers containing values - “Athos”, “Porthos” and “Aramis”
+// Add the “D’Artagnan” value to the array
+// Remove Aramis
+
+
+
+var musketeers = ["Athos", "Porthos", "Aramis"];
+musketeers.push("D’Artagnan")
+musketeers.splice(2,1)
+
+
+//Problem statement#
+// Write a function that takes as input an array and calculates the sum of its elements. Your function should also handle negative values.
+
+let input1 = [3, 11, 7, 2, 9, 10];
+let input2 = [-1, 110, 1, 1, 0];
+
+function sumArray(values){ 	
+    let sum = 0;
+  	for (value of values){
+    		sum += value;
+    	}
+ 		console.log(sum);
+}
+sumArray(input1);
+sumArray(input2);
+
+// --------------//
+
+// Problem statement#
+// Write a program that creates an array of the following elements, then returns the maximum element in the array.
+
+let input1 = [3, 11, 7, 2, 9, 10];
+let input2 = [-1,-2,-100,3];
+
+function maxElement(values){
+  	var max = values[0];
+  	for (value of values){
+    		if(value > max) 
+			{
+				max = value;
+			}
+			
+    	}
+ 		console.log(max);
+}
+maxElement(input1);
+maxElement(input2);
+
+
+// Problem statement#
+// Write a program that creates an array of the following elements, then returns the  minimum element.
+
+function secondMinimum(values){
+  sndMin = values [0];
+    for (value of values)
+    {
+        if (value < sndMin)
+        {
+            sndMin = value;
+        }
+    }
+  return sndMin;
+}
+
+
+
+
+
+//-------------------------------------------//
+
+const name = "adil"
+for (let i = 0; i < name.length; i++)
+{
+    console.log(name[i])
+}
+
+const name='adil'
+for (const letter of name)
+{
+    console.log(letter)
+}
+
+
+// ------------------------------- //
+
+
+const monthList = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec";
+const months = monthList.split(","); // `,` is the separator in monthList.
+console.log(months)
+//months is the array containing substrings at distinct array indices
+
+// -----//
+
+
+const name = 'adil'
+console.log(name.indexOf('i'))
+
+const names = "hello adil"
+console.log(names.endsWith('adil')) // true
+
+
+// -------------------- //
+
+// Problem statement#
+// Write a program that returns the length of a given word, and its lowercase, and uppercase version.
+
+
+function length(word)
+{
+    return word.length
+}
+
+length('hello')
+function lowerCase(word){
+    return word.toLowerCase()
+}
+
+function upperCase(word){
+    return word.toUpperCase()
+}
